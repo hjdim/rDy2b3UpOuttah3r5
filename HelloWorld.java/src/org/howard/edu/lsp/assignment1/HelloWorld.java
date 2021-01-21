c936414b41b0de906578021399888c32ae6248f8
Joshua_Hector <joshua.hector@bison.howard.edu>
2021-01-20 20:31:59 -0500

    commit Hello World Assignment

---
+++ HelloWorld.java/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,12 @@
+//Declaring Howard University Object Oriented Programming SP2021 Package - Assignment One
+package org.howard.edu.lsp.assignment1;
+
+//Hello World class 
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+			System.out.println("Hello World!!");
+	
+	}
+	
+}
